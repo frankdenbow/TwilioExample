5b4a6288c273e5f044ac0c99ee21025f7ce03579
Frank Denbow <frank.denbow@gmail.com>
2011-04-16 05:29:17 -0400

    putting changes

--- index.rb
+++ index.rb
@@ -2,9 +2,13 @@ require "rubygems"
 require "sinatra"
 require "twiliolib"
 
-get '/' do
+post '/' do
   @output = Twilio::Response.new
   
-  incoming = params[:Body]
+  incoming = params[:Body].nil? ? params[:Body] : " Nothing"
   @sms= Twilio::Sms.new("Hey! You just said: "+ incoming)
+  
+  @output.append(@sms)
+  content_type "text/xml"
+  @output.respond
 end
