ca28a36115c6945ebe5e3146bc9b09930c520795
Frank Denbow <frank.denbow@gmail.com>
2011-04-16 05:47:40 -0400

    adding counter

--- index.rb
+++ index.rb
@@ -2,15 +2,20 @@ require "rubygems"
 require "sinatra"
 require "twiliolib"
 
+enable :sessions
+
 get '/' do
+  session['counter'] ||= 0
+  session['counter'] += 1
+  
   @output = Twilio::Response.new
   
   incoming = params[:Body].nil? ?  " Nothing" : params[:Body].to_s
   
   if incoming.include? "#sexy"
-    @sms = Twilio::Sms.new("You are one sexy mofo")
+    @sms = Twilio::Sms.new("You are one sexy mofo" + "Count(" + session['counter']+ ")")
   else
-    @sms= Twilio::Sms.new("Hey! You're not a sexy mofo. You said: "+ incoming)
+    @sms= Twilio::Sms.new("Hey! You're not a sexy mofo. You said: "+ incoming + "Count(" + session['counter']+ ")")
   end
   
   @output.append(@sms)
